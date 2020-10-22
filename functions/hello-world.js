a72c1f848e21afa4f0f76b69df6117e563271aa3
Prasanna150692 <73257788+Prasanna150692@users.noreply.github.com>
2020-10-22 18:25:37 +0530

    Change 1

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,6 +1,6 @@
 exports.handler = function(context, event, callback) {
     const result = {
-        message : "Hello World!"
+        message : "Hello World! Welcome to Twilio Functions"
     }
 
     callback(null, result);
